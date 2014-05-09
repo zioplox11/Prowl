aa384783038a071a04b88094b1c7fd9eb055263e
zioplox11 <joshua.guthals@gmail.com>
2014-05-09 15:29:41 -0400

    adding some more basic rails schtuff

--- db/migrate/20140509185202_create_users.rb
+++ db/migrate/20140509185202_create_users.rb
@@ -4,11 +4,14 @@ class CreateUsers < ActiveRecord::Migration
       t.string :first_name
       t.string :last_name
       t.string :nickname
-      t.string :
+      t.string :password_digest
       t.integer :height_feet
       t.integer :height_inches
       t.string :body_type
       t.integer :age
+      t.string :email
+      t.boolean    :admin_status, default: false
+      t.timestamps
     end
   end
 end
--- readme.md
+++ readme.md
@@ -1,10 +1,22 @@
-###Prowl
+###Prowl v0.1.0
 
 #####A lesbian dating, sex, friendship, and social networking app for computer or smartphone.
 
 ######Helping you find your cat's meow.
 
+Link to ERD
 
+Link to Pivotal Tracker
+
+Link to app on Heroku
+
+Current Specs
+
+Development Team:
+
+Rob White | Joshua Guthals
+
+Design by: Atanas Djibirov
 
 
 
