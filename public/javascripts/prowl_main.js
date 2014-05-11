c52be123e6009a3a6668e7b9a88098cde41974a9
zioplox11 <joshua.guthals@gmail.com>
2014-05-11 17:33:22 -0400

    adding one page initial js functionality

--- public/javascripts/prowl_main.js
+++ public/javascripts/prowl_main.js
@@ -11,10 +11,33 @@ $(function(){
 
   console.log(helloLesbianWorld());
 
-  var firstPost = $("#localLesbians").html('<p>TESTING IT OUT!</p>');
+  var mainView = $("#main_view").html('<p>TESTING IT OUT!</p>');
 
-  var content = $('<p>').html(helloLesbianWorld());
+  var testContent = $('<p>').html(helloLesbianWorld());
 
-  firstPost.append(content);
+  mainView.append(testContent);
+
+  var addingContent = $("#main_view");
+
+  var signUp = $("<div id='login_signup'><input type='text' placeholder='Enter Name Here' class='textSignUp'></br><input type='text' placeholder='Enter Password Here' class='textPassword'><button class='passwordButton'>Sign Up Now!</button></br></div>");
+
+  var peopleOnline =   $("<div id='local_users'>");
+
+  var someoneOnline = $('<div class="local_user"><p>A local user</p><p>Name: Barbara</p><p>Age: 33</div>');
+
+  var someoneElseOnline = $('<div class="local_user"><p>A local user</p><p>Name: Sam</p><p>Age: 23</div>');
+
+  var someoneElseOnline1 = $('<div class="local_user"><p>A local user</p><p>Name: Lauryn</p><p>Age: 26</div>');
+
+  peopleOnline.append(someoneOnline);
+
+  addingContent.append(signUp);
+
+  addingContent.append(peopleOnline);
+
+  peopleOnline.append(someoneElseOnline);
+
+  peopleOnline.append(someoneElseOnline1);
+
 
 });
