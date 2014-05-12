7e52f924ced44fe1302f460158295849f6b23444
zioplox11 <joshua.guthals@gmail.com>
2014-05-12 01:16:55 -0400

    building extra fake users to work on scrolling on interior views

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -5,34 +5,49 @@ var app = {
 
 $(function(){
 
-  // var mainView = $("#main_view").html('<p id="tester">Main View!</p>');
+  //here is the view to uncomment if you ARE LOGGED in
 
-  // var addingContent = $("#main_view");
+ //  var mainView = $("#main_view").html('<p id="tester">Main View!</p>');
 
-  var mainSplashView = $('#main_splash_view');
+ //  var addingContent = $("#main_view");
+
+ //  var peopleOnline =   $("<div id='local_users'>");
+
+ //  var someoneOnline = $('<div class="local_user"><p>A local user</p><p>Name: Barbara</p><p>Age: 33</div>');
+ //  var someoneElseOnline = $('<div class="local_user"><p>A local user</p><p>Name: Sam</p><p>Age: 23</div>');
+ //  var someoneElseOnline1 = $('<div class="local_user"><p>A local user</p><p>Name: Lauryn</p><p>Age: 26</div>');
+ //  var someoneOnline1 = $('<div class="local_user"><p>A local user</p><p>Name: Elaine</p><p>Age: 43</div>');
+ //  var someoneElseOnline2 = $('<div class="local_user"><p>A local user</p><p>Name: Terry</p><p>Age: 27</div>');
+ //  var someoneElseOnline3 = $('<div class="local_user"><p>A local user</p><p>Name: Amanda</p><p>Age: 26</div>');
+ //  var someoneElseOnline4 = $('<div class="local_user"><p>A local user</p><p>Name: Minnie</p><p>Age: 36</div>');
+ //  var someoneElseOnline5 = $('<div class="local_user"><p>A local user</p><p>Name: Lonnie</p><p>Age: 30</div>');
+ // var someoneElseOnline6 = $('<div class="local_user"><p>A local user</p><p>Name: Rennie</p><p>Age: 23</div>');
+
+ //  peopleOnline.append(someoneOnline);
+ //  addingContent.append(peopleOnline);
+ //  peopleOnline.append(someoneElseOnline);
+ //  peopleOnline.append(someoneElseOnline1);
+ //  peopleOnline.append(someoneOnline1);
+ //  peopleOnline.append(someoneElseOnline2);
+ //  peopleOnline.append(someoneElseOnline3);
+ //  peopleOnline.append(someoneElseOnline4);
+ //  peopleOnline.append(someoneElseOnline5);
+ //  peopleOnline.append(someoneElseOnline6);
+
+// here is the view to uncomment if you are NOT logged in:
 
 
-  var signUp = $("<div id='signup_form'><p>Welcome to Prowl! The best place to mix and mingle with local lesbians ... and those around the world!</p><input type='text' placeholder='Enter Name Here' class='name_signup'></br><input type='text' placeholder='Enter Email Here' class='email_signup'></br><input type='password' placeholder='Enter Password Here' class='enter_password'></br><input type='password' placeholder='Confirm Password Here' class='confirm_password'></br><button class='submit_button'>Sign Up Now!</button></br></div>");
+
+var mainSplashView = $('#main_splash_view');
+
+  var signUp = $("<div id='signup_form'><p>The best place to mix and mingle with ladies locally ... and around the world!</p><input type='text' placeholder='Enter Name Here' class='name_signup'></br><input type='text' placeholder='Enter Email Here' class='email_signup'></br><input type='password' placeholder='Enter Password Here' class='enter_password'></br><input type='password' placeholder='Confirm Password Here' class='confirm_password'></br><button class='submit_button'>Sign Up Now!</button></br></div>");
 
   mainSplashView.append(signUp);
 
-  // var peopleOnline =   $("<div id='local_users'>");
-
-  // var someoneOnline = $('<div class="local_user"><p>A local user</p><p>Name: Barbara</p><p>Age: 33</div>');
-
-  // var someoneElseOnline = $('<div class="local_user"><p>A local user</p><p>Name: Sam</p><p>Age: 23</div>');
-
-  // var someoneElseOnline1 = $('<div class="local_user"><p>A local user</p><p>Name: Lauryn</p><p>Age: 26</div>');
-
-  // peopleOnline.append(someoneOnline);
-
-  // addingContent.append(peopleOnline);
-
-  // peopleOnline.append(someoneElseOnline);
-
-  // peopleOnline.append(someoneElseOnline1);
-
-  // user = new User();
+  user = new User();
 
   signUpView = new SignUpView();
+
+
+
 });
