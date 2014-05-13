1f68e7094951e7e5983bd0ce4134f764df781302
zioplox11 <joshua.guthals@gmail.com>
2014-05-13 14:44:20 -0400

    adding additional user details to profile view

--- app/assets/javascripts/login_view.js
+++ app/assets/javascripts/login_view.js
@@ -14,14 +14,42 @@ LogInView = Backbone.View.extend({
 
   renderProfile: function(){
     var pr = $('<div id="my_profile">');
-    pr.html(this.model.get("username"));
+    pr.html(this.model.get(attributes()));
     this.$el.append(pr);
   },
 
   refreshProfile: function(){
      var pr = $('<div id="my_profile">');
-    pr.html(this.model.get("username"));
+    pr.html(this.model.get(attributes()));
     this.$el.append(pr);
+  },
+
+  attributes: function(){
+    this.model.get("username");
+    this.model.get("gender");
+    this.model.get("age");
+    this.model.get("height");
+    this.model.get("body_type");
+    this.model.get("birthdate");
+    this.model.get("email");
+    this.model.get("ethnicity");
+    this.model.get("diet");
+    this.model.get("drugs");
+    this.model.get("drinks");
+    this.model.get("smokes");
+    this.model.get("zodiac_sign");
+    this.model.get("education");
+    this.model.get("job");
+    this.model.get("income");
+    this.model.get("relationship_status");
+    this.model.get("relationship_type");
+    this.model.get("children_family");
+    this.model.get("languages");
+    this.model.get("pets");
+    this.model.get("self_summary");
+    this.model.get("looking_for");
+    this.model.get("created_at");
+    this.model.get("updated_at");
   }
 
 });
