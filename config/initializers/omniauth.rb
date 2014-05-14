a19e274c4db08b11f4fb8690f7a0712285c1ff87
zioplox11 <joshua.guthals@gmail.com>
2014-05-14 18:56:54 -0400

    css adjusting on main page background

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET'], :display => 'popup'
+  provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
 end
