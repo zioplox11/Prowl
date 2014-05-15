dff41ac032a24c75cba8d464661f3d0d091352ed
Rob <robert.emery1@gmail.com>
2014-05-15 18:08:50 -0400

    added profile_image_url to users table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140514130235) do
+ActiveRecord::Schema.define(version: 20140515220033) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20140514130235) do
     t.string   "fb_token"
     t.integer  "fb_expiration"
     t.integer  "zip"
+    t.text     "profile_image_url"
   end
 
 end
